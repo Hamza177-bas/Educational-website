d3e4ebb6eeac198bc1d0bb78b669c3e4b7a991a8
Hamza <hamzacoude@gmail.com>
2024-12-11 15:14:00 +0100

    add fonction affecher databesse (vill and continent)

--- Admin.php
+++ Admin.php
@@ -11,16 +11,30 @@
 <?php 
 include('conn.php');
 
-$query = mysqli_query($conn, "SELECT * FROM `pays`");
+$pays = mysqli_query($conn, "SELECT * FROM `pays`");
 
 
-while ($row = mysqli_fetch_array($query)) {
+$vill =mysqli_query($conn,"SELECT * FROM `vill`");
+$continent =mysqli_query($conn,"SELECT * FROM `continent`");
+while ($row = mysqli_fetch_array($pays)) {
     echo "ID: " . $row['id'] . "<br>";
     echo "Name: " . $row['name'] . "<br>";
     echo "Population: " . $row['population'] . "<br>";
     echo "Languages: " . $row['languages'] . "<br>";
     echo "Continent ID: " . $row['id_continent'] . "<br><br>";
 }
+   echo "<h2> VILL </h2>";
+    while($row =mysqli_fetch_array($vill)){
+        echo "ID: " . $row['id'] . "<br>";
+        echo "Name: " . $row['name'] . "<br>";
+        echo "type: " . $row['type'] . "<br>";
+        echo "id_pays: " . $row['id_pays'] . "<br>" ."<br>";
+    }
+   echo "<h2> Continent </h2>";
+   while($row =mysqli_fetch_array($continent)){
+        echo "ID: " . $row['id'] . "<br>";
+        echo "Name: " . $row['name'] . "<br>";
+   }
 ?>
 
 
